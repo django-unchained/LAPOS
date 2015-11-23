1c10dffdd63fb5998263320e28deda8a88692ed1
Emily Tagtow <emily.tagtow@utexas.edu>
2015-11-23 18:51:58 -0500

    small fix

--- pos-tagging/parse-common.py
+++ pos-tagging/parse-common.py
@@ -13,17 +13,19 @@ def convertTags(filename):
     tagDict[oldTag] = newTag.strip()
   return tagDict
 
-def tagWrite(line, tagDict, tagger, langCode, output):
+def tagWrite(line, tagDict, tagger, langCode, output, errors):
   tags = tagger.tag(line)
   splitLine = line.split()
   output.write("\t".join(splitLine)+"\n")
   for wordTags in tags:
     for tag in wordTags[1:]:
-      oldTag, word, prob = tag.split()
+      #print tag
+      oldTag = tag.split()[0]
+      prob = tag.split()[-1]
       try:
         universalTag = tagDict[oldTag]
       except KeyError:
-        print oldTag, "not found in", langCode
+        errors.write(oldTag+" not found in "+langCode+"tag dictionary.\n")
         universalTag = oldTag
         #universalTag = raw_input("Enter the universal tag for "+oldTag+"in "+langCode+": ")
         #tagDict[oldTag] = universalTag
@@ -60,54 +62,58 @@ def tagLang(langs, corpus):
   engTagDict = convertTags('en-ptb.map')
   corpus = open(corpus, 'r')
   output = open('tagged.all','w')
+  errors = open('errors.txt','w')
   numLines = 0
 
   for line in corpus:
+    #numLines += 1
+    #print numLines
     if line.startswith("<fr>"):
       splitline = line.split(" ")
       notag = " ".join(splitline[1:])
-      tagWrite(notag, frTagDict, frTagger,"fr", output)
+      tagWrite(notag, frTagDict, frTagger,"fr", output, errors)
     elif line.startswith("<sl>"):
       splitline = line.split(" ")
       notag = " ".join(splitline[1:])
-      tagWrite(notag, slTagDict, slTagger,"sl", output)
+      tagWrite(notag, slTagDict, slTagger,"sl", output, errors)
     elif line.startswith("<de>"):
       splitline = line.split(" ")
       notag = " ".join(splitline[1:])
-      tagWrite(notag, deTagDict, deTagger,"de", output)
+      tagWrite(notag, deTagDict, deTagger,"de", output, errors)
     elif line.startswith("<it>"):
       splitline = line.split(" ")
       notag = " ".join(splitline[1:])
-      tagWrite(notag, itTagDict, itTagger,"it", output)
+      tagWrite(notag, itTagDict, itTagger,"it", output, errors)
     elif line.startswith("<pl>"):
       splitline = line.split(" ")
       notag = " ".join(splitline[1:])
-      tagWrite(notag, plTagDict, plTagger,"pl", output)
+      tagWrite(notag, plTagDict, plTagger,"pl", output, errors)
     elif line.startswith("<sk>"):
       splitline = line.split(" ")
       notag = " ".join(splitline[1:])
-      tagWrite(notag, skTagDict, skTagger,"sk", output)
+      tagWrite(notag, skTagDict, skTagger,"sk", output, errors)
     elif line.startswith("<es>"):
       splitline = line.split(" ")
       notag = " ".join(splitline[1:])
-      tagWrite(notag, esTagDict, esTagger,"es", output)
+      tagWrite(notag, esTagDict, esTagger,"es", output, errors)
     elif line.startswith("<nl>"):
       splitline = line.split(" ")
       notag = " ".join(splitline[1:])
-      tagWrite(notag, nlTagDict, nlTagger,"nl", output)
+      tagWrite(notag, nlTagDict, nlTagger,"nl", output, errors)
     elif line.startswith("-1") or line[0].isdigit():
       splitline = line.split(",")
       output.write("\t".join(splitline))
     elif line.startswith("!@#$%^&*()"):
       output.write(line)
     else:
-      tagWrite(line,engTagDict,engTagger,"en", output)
+      tagWrite(line,engTagDict,engTagger,"en", output, errors)
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.usage = "python treetagger-test.py -l fr sl -i common.txt"
-  parser.add_argument("-l", "--langs", dest="langs", action="store", nargs='+', help="Language codes for languages to tag")
+  parser.usage = "python treetagger-test.py -i common.txt"
+  #parser.add_argument("-l", "--langs", dest="langs", action="store", nargs='+', help="Language codes for languages to tag")
   parser.add_argument("-i", "--infile", dest="corpus", action="store", help="File to tag")
   opts = parser.parse_args()
-  tagLang(opts.langs, opts.corpus)
+  langs = ["fr","sl","de","it","pl","sk","es","nl"]
+  tagLang(langs, opts.corpus)
 main()
